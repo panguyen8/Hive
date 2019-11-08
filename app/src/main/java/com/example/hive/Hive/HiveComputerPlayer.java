f9f9b7288213b00f1eac0414da2b323e749fd537
Phuocan Nguyen <phuocan.nguyen8@gmail.com>
2019-11-07 17:39:57 -0800

    Updates to human player and computer player

--- app/src/main/java/com/example/hive/Hive/HiveComputerPlayer.java
+++ app/src/main/java/com/example/hive/Hive/HiveComputerPlayer.java
@@ -3,4 +3,5 @@ package com.example.hive.Hive;
 import com.example.hive.game.GameComputerPlayer;
 
 public class HiveComputerPlayer extends GameComputerPlayer {
+    public HiveComputerPlayer(String name){super(name);};
 }
--- app/src/main/java/com/example/hive/Hive/HiveHumanPlayer.java
+++ app/src/main/java/com/example/hive/Hive/HiveHumanPlayer.java
@@ -3,4 +3,7 @@ package com.example.hive.Hive;
 import com.example.hive.game.GameHumanPlayer;
 
 public class HiveHumanPlayer extends GameHumanPlayer {
+    public HiveHumanPlayer(String name) {
+        super(name);
+    }
 }
