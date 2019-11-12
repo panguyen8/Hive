386fc6355ea02c88b3d5e95aa82bfb834dc162b2
Phuocan Nguyen <phuocan.nguyen8@gmail.com>
2019-11-12 08:20:11 -0800

    Hive Human Player Updates

--- app/src/main/java/com/example/hive/Hive/HiveView.java
+++ app/src/main/java/com/example/hive/Hive/HiveView.java
@@ -9,13 +9,21 @@ import android.view.SurfaceView;
 
 public class HiveView extends SurfaceView {
 
+    protected HiveGameState state;
+
     Paint hexagonalPaint = new Paint();
+    Paint hexagonalTargetPaint = new Paint();
 
     public HiveView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
         hexagonalPaint.setColor(Color.WHITE);
+        hexagonalTargetPaint.setColor(Color.RED);
+    }
+
+    public void setState(HiveGameState state) {
+        this.state = state;
     }
 
     public void onDraw(Canvas canvas)  {
@@ -37,5 +45,14 @@ public class HiveView extends SurfaceView {
         canvas.drawLine(startX+100, startY+66, startX+50, startY+100, hexagonalPaint);
         canvas.drawLine(startX+50, startY+100, startX, startY+66, hexagonalPaint);
         canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalPaint);
+    }
+
+    public void drawTargetHexagon(Canvas canvas, int startX, int startY) {
+        canvas.drawLine(startX, startY+33, startX+50, startY, hexagonalTargetPaint);
+        canvas.drawLine(startX+50, startY, startX+100, startY+33, hexagonalTargetPaint);
+        canvas.drawLine(startX+100, startY+33, startX+100, startY+66, hexagonalTargetPaint);
+        canvas.drawLine(startX+100, startY+66, startX+50, startY+100, hexagonalTargetPaint);
+        canvas.drawLine(startX+50, startY+100, startX, startY+66, hexagonalTargetPaint);
+        canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalTargetPaint);
     }
     }
