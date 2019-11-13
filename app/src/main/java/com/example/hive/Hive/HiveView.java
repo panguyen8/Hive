29d69459779b86d782eebf2f6db69534f7619b77
hilderbr21 <54870346+hilderbr21@users.noreply.github.com>
2019-11-12 23:10:46 -0800

    Draws Hexagons with Path class.

--- app/src/main/java/com/example/hive/Hive/HiveView.java
+++ app/src/main/java/com/example/hive/Hive/HiveView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
@@ -15,12 +16,25 @@ public class HiveView extends SurfaceView {
     Paint hexagonalPaint = new Paint();
     Paint hexagonalTargetPaint = new Paint();
 
+    Path Hexagon = new Path();
+    Point point1 = new Point();
+    Point point2 = new Point();
+    Point point3 = new Point();
+    Point point4 = new Point();
+    Point point5 = new Point();
+    Point point6 = new Point();
+
     public HiveView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
         hexagonalPaint.setColor(Color.WHITE);
         hexagonalTargetPaint.setColor(Color.RED);
+
+        hexagonalPaint.setStyle(Paint.Style.FILL);
+        hexagonalTargetPaint.setStyle(Paint.Style.STROKE);
+
+
     }
 
     public void setState(HiveGameState state) {
@@ -57,20 +71,44 @@ public class HiveView extends SurfaceView {
     }
 
     public void drawHexagon(Canvas canvas, int startX, int startY) {
-        canvas.drawLine(startX, startY+33, startX+50, startY, hexagonalPaint);
+        /*canvas.drawLine(startX, startY+33, startX+50, startY, hexagonalPaint);
         canvas.drawLine(startX+50, startY, startX+100, startY+33, hexagonalPaint);
         canvas.drawLine(startX+100, startY+33, startX+100, startY+66, hexagonalPaint);
         canvas.drawLine(startX+100, startY+66, startX+50, startY+100, hexagonalPaint);
         canvas.drawLine(startX+50, startY+100, startX, startY+66, hexagonalPaint);
-        canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalPaint);
+        canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalPaint);*/
+
+        canvas.drawPath(drawHexagonLines(startX, startY), hexagonalPaint);
     }
 
     public void drawTargetHexagon(Canvas canvas, int startX, int startY) {
-        canvas.drawLine(startX, startY+33, startX+50, startY, hexagonalTargetPaint);
+        /*canvas.drawLine(startX, startY+33, startX+50, startY, hexagonalTargetPaint);
         canvas.drawLine(startX+50, startY, startX+100, startY+33, hexagonalTargetPaint);
         canvas.drawLine(startX+100, startY+33, startX+100, startY+66, hexagonalTargetPaint);
         canvas.drawLine(startX+100, startY+66, startX+50, startY+100, hexagonalTargetPaint);
         canvas.drawLine(startX+50, startY+100, startX, startY+66, hexagonalTargetPaint);
-        canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalTargetPaint);
+        canvas.drawLine(startX, startY+66, startX, startY+33, hexagonalTargetPaint);*/
+
+        canvas.drawPath(drawHexagonLines(startX, startY), hexagonalTargetPaint);
     }
+
+    private Path drawHexagonLines(int x, int y) {
+        point1.set(x, y+33);
+        point2.set(x+50, y);
+        point3.set(x+100, y+33);
+        point4.set(x+100, y+66);
+        point5.set(x+50, y+100);
+        point6.set(x, y+66);
+
+        Hexagon.moveTo(point1.x, point1.y);
+        Hexagon.lineTo(point2.x, point2.y);
+        Hexagon.lineTo(point3.x, point3.y);
+        Hexagon.lineTo(point4.x, point4.y);
+        Hexagon.lineTo(point5.x, point5.y);
+        Hexagon.lineTo(point6.x, point6.y);
+        Hexagon.close();
+
+        return Hexagon;
+
     }
+}
