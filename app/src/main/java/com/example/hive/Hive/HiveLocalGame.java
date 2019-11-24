7b9fbad7bdbe27cfa55a667e27868c9a1f90ef1f
Stephen Nguyen <nguyenst22@up.edu>
2019-11-24 00:08:26 -0800

    Added some comments, including some expansion on class header (we may have lost some easy points from alpha).
    If you are not credited in the authors, but have worked on LocalGame, feel free to add yourselves.

--- app/src/main/java/com/example/hive/Hive/HiveLocalGame.java
+++ app/src/main/java/com/example/hive/Hive/HiveLocalGame.java
@@ -5,12 +5,24 @@ import com.example.hive.game.LocalGame;
 import com.example.hive.game.actionMessage.GameAction;
 
 /**
- * Represents a local game, which is responsible for enforcing rules
+ * Represents a local game, which is responsible for enforcing rules,
+ * moving pieces, etc.
+ * @author Stephen Nguyen
+ * @author Samuel Nguyen
+ * @author Phuocan Nguyen
  */
 public class HiveLocalGame extends LocalGame {
+    //Create instance of current game state
     private HiveGameState hgs;
+
+    //After a player's fourth turn,
+    //the bee must be played if it already
+    //hasn't been
     private int turnCount = 0;
 
+    /**
+     * Constructor of local game
+     */
     public HiveLocalGame() {
         super();
         hgs = new HiveGameState();
@@ -108,6 +120,9 @@ public class HiveLocalGame extends LocalGame {
         if (action instanceof HiveMoveAction) {
             HiveMoveAction move = (HiveMoveAction) action;
 
+            //To prevent moving opponent's pieces, make sure only player's
+            //own pieces are being checked, return false if an opponent's
+            //piece was selected
             if (hgs.getTurn() == 0) {
                 if(hgs.board[move.startRow][move.startCol] == HiveGameState.piece.WBEE ||
                         hgs.board[move.startRow][move.startCol] == HiveGameState.piece.WBEETLE ||
@@ -116,6 +131,9 @@ public class HiveLocalGame extends LocalGame {
                         hgs.board[move.startRow][move.startCol] == HiveGameState.piece.WGHOPPER) {
                     return false;
                 }
+
+                //Moving can only be done on legal spaces
+                //Moving rules will be done here as well
                 if (hgs.board[move.endRow][move.endCol] == HiveGameState.piece.TARGET)
                 {
                     if(hgs.board[move.startRow][move.startCol] == HiveGameState.piece.WBEE ||
@@ -142,9 +160,13 @@ public class HiveLocalGame extends LocalGame {
                     hgs.board[move.endRow][move.endCol] = hgs.board[move.startRow][move.startCol];
                     hgs.board[move.startRow][move.startCol] = HiveGameState.piece.EMPTY;
                 }
+
+                //Set the turn to the next player
                 hgs.setTurn(1);
             }
             else {
+                //This functions like the code above, just with regards to
+                //the other player
                 if(hgs.board[move.startRow][move.startCol] == HiveGameState.piece.BBEE ||
                         hgs.board[move.startRow][move.startCol] == HiveGameState.piece.BBEETLE ||
                         hgs.board[move.startRow][move.startCol] == HiveGameState.piece.BANT ||
@@ -185,6 +207,7 @@ public class HiveLocalGame extends LocalGame {
             //increment turnCount
             turnCount++;
         }
+
         // Placing piece
         else if (action instanceof HivePlacePieceAction) {
             HivePlacePieceAction placement = (HivePlacePieceAction) action;
@@ -197,6 +220,8 @@ public class HiveLocalGame extends LocalGame {
                 return true;
             }
 
+            //A piece can only be placed if it is in the
+            //player's hand, represented by the bugList (ArrayList)
             if (!hgs.bugList.contains(((HivePlacePieceAction) action).piece)){
                 return false;
             }
@@ -211,11 +236,17 @@ public class HiveLocalGame extends LocalGame {
 
                     hgs.board[placement.row][placement.col] = ((HivePlacePieceAction) action).piece;
                     hgs.bugList.remove(((HivePlacePieceAction) action).piece);
-                } else {
+                }
+
+                else {
                     return false;
                 }
+
+                //Set next turn
                 hgs.setTurn(1);
             }
+
+            //Same as above, just with other player's pieces
             else {
                 //makes sure that the bee is placed
                 if(turnCount > 7 && hgs.bugList.contains(HiveGameState.piece.BBEE)) {
@@ -226,7 +257,9 @@ public class HiveLocalGame extends LocalGame {
                 if(hgs.board[placement.row][placement.col] == HiveGameState.piece.TARGET) {
                     hgs.board[placement.row][placement.col] = ((HivePlacePieceAction) action).piece;
                     hgs.bugList.remove(((HivePlacePieceAction) action).piece);
-                } else {
+                }
+
+                else {
                     return false;
                 }
                 hgs.setTurn(0);
@@ -238,11 +271,15 @@ public class HiveLocalGame extends LocalGame {
             turnCount++;
             return true;
         }
+
         // Selecting a piece
         else if(action instanceof HiveButtonAction) {
+            //A piece can only be selected if it is in the ArrayList
+            //of unplayed pieces
             if (!hgs.bugList.contains(((HiveButtonAction) action).gamePiece)) {
                 return false;
             }
+
             for (int row = 1; row < hgs.board.length - 1; row++)
             {
                 for (int col = 1; col < hgs.board[col].length - 1; col++)
@@ -255,16 +292,21 @@ public class HiveLocalGame extends LocalGame {
         //selected piece targets
         else if (action instanceof HiveSelectedPieceAction) {
             HiveSelectedPieceAction move = (HiveSelectedPieceAction) action;
+
+            //An empty spot can't be selected (there is no piece)
             if (hgs.board[move.row][move.col] == HiveGameState.piece.EMPTY) {
                 return false;
             }
+
             for (int row = 1; row < hgs.board.length - 1; row++)
             {
                 for (int col = 1; col < hgs.board[col].length - 1; col++)
                 {
                     if (row == move.row && col == move.col) {
                         //do nothing
-                    } else {
+                    }
+
+                    else {
                         hgs.makeTarget(row, col);
                     }
                 }
@@ -273,6 +315,7 @@ public class HiveLocalGame extends LocalGame {
         }
         // Resetting the board
         else if (action instanceof HiveResetBoardAction) {
+            //Iterate through board and make each spot empty
             for (int i = 0; i < hgs.board.length; i++)
             {
                 for (int j = 0; j < hgs.board[i].length; j++)
@@ -284,8 +327,12 @@ public class HiveLocalGame extends LocalGame {
             //hgs.board[5][5] = HiveGameState.piece.WBEE;
             //hgs.board[4][6] = HiveGameState.piece.BBEE;
 
+            //Reset turn count to 0
             turnCount = 0;
 
+            //To make adding all bugs to the player
+            //hands easier, clear it, then add the
+            //appropriate quantity of each piece
             hgs.bugList.clear();
 
             //1 BBEE, 2 BSPIDERS, 3 BANT, 3 BGHOPPER, 2,BBEETLE
@@ -319,22 +366,37 @@ public class HiveLocalGame extends LocalGame {
 
     // Below methods are helpers that dictate movement of each piece
 
+    /**
+     * Checks the spots that are legal for a bee to move
+     */
     public void moveBee() {
 
     }
 
+    /**
+     * Checks spots for beetle
+     */
     public void moveBeetle() {
 
     }
 
+    /**
+     * Checks spots for ants
+     */
     public void moveAnt() {
 
     }
 
+    /**
+     * Checks spots for spiders
+     */
     public void moveSpider() {
 
     }
 
+    /**
+     * Checks spots for grasshoppers
+     */
     public void moveGHopper() {
 
     }
