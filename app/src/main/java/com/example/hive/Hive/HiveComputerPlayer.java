00bc90cbb04d8fff11e795dfbec5018c6bc43dc8
hilderbr21 <54870346+hilderbr21@users.noreply.github.com>
2019-12-07 00:49:31 -0800

    Revert "reverted fixed islands ai"
    This reverts commit c6a2b40dc9c1a2592f28277c87ac204a0d41514d.

--- app/src/main/java/com/example/hive/Hive/HiveComputerPlayer.java
+++ app/src/main/java/com/example/hive/Hive/HiveComputerPlayer.java
@@ -384,7 +384,7 @@ public class HiveComputerPlayer extends GameComputerPlayer {
                             if (test.board[i][j] == HiveGameState.piece.TARGET) {
                                 spaceCount += 1;
                             }
-                            if (spaceCount == randomLocation) {
+                            if (spaceCount == randomLocation && test.board[i][j] == HiveGameState.piece.TARGET) {
                                 HivePlacePieceAction placePiece = new HivePlacePieceAction(this, i, j, hand.get(randomBug));
                                 game.sendAction(placePiece);
                                 return;
